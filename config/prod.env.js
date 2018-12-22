944aa1feddf77bb82abd68261347b2ae7d2a875c
Matthew J. Lewis <lightscalar@gmail.com>
2018-12-21 19:50:33 -0500

    Added protocol to the environment variables.

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  ROOT_API: '"argos.michaero.com:2005"',
-  IMAGE_API: '"argos.michareo.com:2007"'
+  ROOT_API: '"http://argos.michaero.com:2005"',
+  IMAGE_API: '"http://argos.michareo.com:2007"'
 }
